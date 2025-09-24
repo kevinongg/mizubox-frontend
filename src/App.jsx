fb8b58a9e7e54f5f6a0c68ca0d77a5431bb9aa6c
Kevin Ong <kevinongg@gmail.comm>
2025-09-23 22:44:03 -0400

    add route to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
+
+// Auth
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import ProtectedRoutes from "./auth/ProtectedRoutes";
-import Error404 from "./Error404";
+
+// Public pages
+import HomePage from "./pages/HomePage";
+import Error404 from "./pages/Error404";
+
+// Protected pages
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<p>Home page</p>} />
+        <Route index element={<HomePage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
 
         <Route element={<ProtectedRoutes />}>
-          <Route />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
 
         <Route path="*" element={<Error404 />} />
