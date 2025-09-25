c1b37ad012c9da5507b91b9b6b4c90135a282dbf
Kevin Ong <kevinongg@gmail.comm>
2025-09-24 22:47:42 -0400

    add persistent cart functionality and cart helper functions

--- src/pages/cart/CartContext.jsx
+++ src/pages/cart/CartContext.jsx
@@ -1,9 +1,36 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const value = {};
+  const [cart, setCart] = useState(() => {
+    // get localstorage, json.parse the string so the savedCart initializes as objects in state
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  // useEffect, json.stringify to turn cart into a string
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  //addToCart function
+  const addToCart = (item) => {
+    setCart((prev) => [...prev, item]);
+  };
+  //removeFromCart function
+  const removeFromCart = (id) => {
+    setCart((prev) => {
+      return prev.filter((i) => i.id !== id);
+    });
+  };
+
+  //clearCart function
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const value = { cart, addToCart, removeFromCart, clearCart };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
 
