6bcc982e189a299c803390b41afe624628dcc9c1
Twinkle <twinkledevwanshi93@gmail.com>
2025-09-29 00:39:44 -0700

    added className

--- src/pages/Menu/BuildYourOwn.jsx
+++ src/pages/Menu/BuildYourOwn.jsx
@@ -1,30 +1,38 @@
-import { useState } from "react";
+// import { useState } from "react";
 import useQuery from "../../api/useQuery";
 
 const BuildYourOwn = () => {
   const { data: nigiris, loading, error } = useQuery("/nigiris", "nigiris");
-  const [selection, setSelection] = useState();
+  // const [selection, setSelection] = useState();
   console.log(nigiris);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Failed to load nigiris</p>;
 
   return (
-    <div>
+    <div className="build-container">
       <h1>Build Your Own Omakase Box</h1>
       <p>Select at least 14 nigiris to continue</p>
-      <>
+<<<<<<< Updated upstream
+      <></>
+=======
+
+      <div className="nigiri-grid">
         {nigiris?.map((nigiri) => {
           return (
             <div key={nigiri.id}>
-              <li>{nigiri.name}</li>
               <li>
                 <img src={nigiri.image_url} alt={nigiri.name} />
               </li>
+              <div className="nigiri-info">
+                <h3>{nigiri.name}</h3>
+                <p>${nigiri.price}</p>
+              </div>
             </div>
           );
         })}
-      </>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 };
