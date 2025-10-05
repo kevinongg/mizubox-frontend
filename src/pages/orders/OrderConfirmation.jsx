3238d005f4ed23e97ac9dc6eb6ad599ba5c95d87
Twinkle <twinkledevwanshi93@gmail.com>
2025-10-05 14:01:24 -0700

    added class and style in this file

--- src/pages/orders/OrderConfirmation.jsx
+++ src/pages/orders/OrderConfirmation.jsx
@@ -2,6 +2,7 @@ import { useParams, Link } from "react-router";
 import { useAccount } from "../account/AccountContext";
 import useQuery from "../../api/useQuery";
 import formatDate from "../../utils/formatDate";
+import "./OrderConfirmation.css";
 
 const OrderConfirmation = () => {
   const { user } = useAccount();
@@ -13,117 +14,120 @@ const OrderConfirmation = () => {
   } = useQuery(`/orders/${publicOrderId}`, "");
   console.log(order);
 
-  if (error) return <p>Failed to load order confirmation page</p>;
+  if (error) return <p className="error-message">Failed to load order confirmation page</p>;
 
   return (
-    <div>
-      <h1>Thank you for your order, {user.name}!</h1>
-      <p>Your order number is: {order?.order_number}</p>
-      <p>Placed on: {formatDate(order?.created_at)}</p>
-      <p>Total: {order?.total_price}</p>
+    <div className="order-confirmation">
+      <div className="order-header">
+        <h1>Thank you for your order, {user.name}!</h1>
+      </div>
+      
+      <div className="order-details">
+        <p id="detail">Your order number is: {order?.order_number}</p>
+        <p>Placed on: {formatDate(order?.created_at)}</p>
+        <p>Total: {order?.total_price}</p>
+      </div>
 
-      <h2>
-        Order Summary ({order?.total_item_count}{" "}
-        {order?.total_item_count > 1 ? "items" : "item"})
-      </h2>
-      <ul>
-        {order?.items.map((item) => {
-          // const orderId = item.order_item_id;
-          const isPreMadeBox = item.boxType === "pre-made";
-          const isCustomBox = item.boxType === "custom";
-          const boxQuantity = item.quantity;
-          const nigiriLength = item.box_details.nigiris?.length;
-          const sauceLength = item.box_details.sauces?.length;
-          const extraLength = item.box_details.extras?.length;
+      <div className="order-summary">
+        <h2>
+          Order Summary ({order?.total_item_count}{" "}
+          {order?.total_item_count > 1 ? "items" : "item"})
+        </h2>
+        <ul className="order-items">
+          {order?.items.map((item) => {
+            // const orderId = item.order_item_id;
+            const isPreMadeBox = item.boxType === "pre-made";
+            const isCustomBox = item.boxType === "custom";
+            const boxQuantity = item.quantity;
+            const nigiriLength = item.box_details.nigiris?.length;
+            const sauceLength = item.box_details.sauces?.length;
+            const extraLength = item.box_details.extras?.length;
 
-          return (
-            <li key={item.order_item_id}>
-              <p>
-                {" "}
-                {isPreMadeBox
-                  ? `Pre-Made Box: ${item.box_details.name}`
-                  : "Custom Omakase Box"}{" "}
-                (×
-                {boxQuantity})
-              </p>
+            return (
+              <li key={item.order_item_id}>
+                <p id="order detail">{" "}
+                  {isPreMadeBox? `Pre-Made Box: ${item.box_details.name}`
+                    : "Custom Omakase Box"}{" "}
+                  (× {boxQuantity})
+                </p>
 
-              {isPreMadeBox && (
-                <div>
-                  {nigiriLength > 0 && (
+                {isPreMadeBox && (
+                  <div className="box-details">
+                    {nigiriLength > 0 && (
+                      <p>
+                        Nigiris:{" "}
+                        {item.box_details.nigiris
+                          .map((nigiri) => `${nigiri.name} (×${nigiri.quantity})`)
+                          .join(`, `)}
+                      </p>
+                    )}
+                  </div>
+                )}
+
+                {isCustomBox && (
+                  <div className="box-details">
                     <p>
-                      Nigiris:{" "}
-                      {item.box_details.nigiris
-                        .map((nigiri) => `${nigiri.name} (×${nigiri.quantity})`)
-                        .join(`, `)}
+                      {" "}Nigiris:{" "}
+                      {nigiriLength > 0 ? item.box_details.nigiris.map(
+                              (nigiri) => `${nigiri.name} (×${nigiri.quantity})`
+                            )
+                            .join(`, `)
+                        : "None"}
                     </p>
-                  )}
-                </div>
-              )}
 
-              {isCustomBox && (
-                <div>
-                  <p>
-                    {" "}
-                    Nigiris:{" "}
-                    {nigiriLength > 0
-                      ? item.box_details.nigiris
-                          .map(
-                            (nigiri) => `${nigiri.name} (×${nigiri.quantity})`
-                          )
-                          .join(`, `)
-                      : "None"}
-                  </p>
+                    <p>
+                      Sauces:{" "}
+                      {sauceLength > 0
+                        ? item.box_details.sauces
+                            .map((sauce) => `${sauce.name} (×${sauce.quantity})`)
+                            .join(`, `)
+                        : "None"}
+                    </p>
 
-                  <p>
-                    Sauces:{" "}
-                    {sauceLength > 0
-                      ? item.box_details.sauces
-                          .map((sauce) => `${sauce.name} (×${sauce.quantity})`)
-                          .join(`, `)
-                      : "None"}
-                  </p>
+                    <p>
+                      Extras:{" "}
+                      {extraLength > 0
+                        ? item.box_details.extras
+                            .map((extra) => `${extra.name} (×${extra.quantity})`)
+                            .join(`, `)
+                        : "None"}
+                    </p>
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
 
-                  <p>
-                    Extras:{" "}
-                    {extraLength > 0
-                      ? item.box_details.extras
-                          .map((extra) => `${extra.name} (×${extra.quantity})`)
-                          .join(`, `)
-                      : "None"}
-                  </p>
-                </div>
-              )}
+        <ul className="additional-items">
+          {order?.sauces.length > 0 && (
+            <li>
+              Sauce:{" "}
+              {order?.sauces
+                .map((sauce) => `${sauce.sauce.name} (×${sauce.quantity})`)
+                .join(", ")}
             </li>
-          );
-        })}
-      </ul>
+          )}
+        </ul>
 
-      <ul>
-        {order?.sauces.length > 0 && (
-          <li>
-            Sauce:{" "}
-            {order?.sauces
-              .map((sauce) => `${sauce.sauce.name} (×${sauce.quantity})`)
-              .join(", ")}
-          </li>
-        )}
-      </ul>
+        <ul className="additional-items">
+          {order?.extras.length > 0 && (
+            <li>
+              Extra:{" "}
+              {order?.extras
+                .map((extra) => `${extra.extra.name} (×${extra.quantity})`)
+                .join(", ")}
+            </li>
+          )}
+        </ul>
+      </div>
 
-      <ul>
-        {order?.extras.length > 0 && (
-          <li>
-            Extra:{" "}
-            {order?.extras
-              .map((extra) => `${extra.extra.name} (×${extra.quantity})`)
-              .join(", ")}
-          </li>
-        )}
-      </ul>
+      <div className="confirmation-message">
+        <p>We are preparing your order now.</p>
+        <p>You will receive an email confirmation shortly.</p>
+      </div>
 
-      <p>We are preparing your order now.</p>
-      <p>You will receive an email confirmation shortly.</p>
-
-      <div>
+      <div className="order-actions">
         <Link to="/orders">Back to Orders</Link>
         <Link to={`/orders/${order?.public_order_id}`}>
           View Full Order Details
